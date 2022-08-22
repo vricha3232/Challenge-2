8e9f6f8e63110cb4652f340fcfbed5789cedda2e
Richa Verma <richa.verma1@publicissapient.com>
2022-08-22 11:46:43 +0530

    Challenge -2

---
+++ Fetch Metadata.ps1
@@ -0,0 +1,6 @@
+﻿Login-AzAccount
+Get-AzSubscription | ft
+$subid = Read-Host -Prompt 'Enter the Subscription Name'
+Select-AzSubscription -Subscription $subid
+$rname = Read-Host -Prompt 'Enter the Resource Name'
+Get-AzResource -Name $rname | ConvertTo-Json | Out-File -FilePath C:\Users\lamalik\Downloads\resourcelist.json
